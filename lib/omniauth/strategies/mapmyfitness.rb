d4d072e567fd3400fb0e9fb021bb18ebc58e3683
Adam Kraft <adamkraft1@gmail.com>
2014-04-26 17:42:47 -0500

    Get uid from 'id' instead of 'user_id'

--- lib/omniauth/strategies/mapmyfitness.rb
+++ lib/omniauth/strategies/mapmyfitness.rb
@@ -5,18 +5,25 @@ module OmniAuth
     class MapMyFitness < OmniAuth::Strategies::OAuth2
       option :name, "mapmyfitness"
 
-      option :client_options, {
-        :site => "https://oauth2-api.mapmyapi.com",
-        :authorize_url => "https://www.mapmyfitness.com/v7.0/oauth2/authorize/",
-        :token_url => "https://oauth2-api.mapmyapi.com/v7.0/oauth2/access_token/",
-        :connection_opts => {
-          :headers => {'Api-Key' => ENV['MMF_API_KEY']}
-        }
-      }
+      # To avoid using ENV variable, move client_options to config/omniauth.rb
+      # For example:
+      #
+      # Rails.application.config.middleware.use OmniAuth::Builder do
+      #   provider :mapmyfitness, PROVIDERS[:mapmyfitness][:consumer_key], PROVIDERS[:mapmyfitness][:consumer_secret], :client_options => { :site => "https://oauth2-api.mapmyapi.com", :authorize_url => "https://www.mapmyfitness.com/v7.0/oauth2/authorize/", :token_url => "https://oauth2-api.mapmyapi.com/v7.0/oauth2/access_token/", :connection_opts => { :headers => {'Api-Key' => PROVIDERS[:mapmyfitness][:consumer_key]} } }
+      # end
+      
+      #option :client_options, {
+      #  :site => "https://oauth2-api.mapmyapi.com",
+      #  :authorize_url => "https://www.mapmyfitness.com/v7.0/oauth2/authorize/",
+      #  :token_url => "https://oauth2-api.mapmyapi.com/v7.0/oauth2/access_token/",
+      #  :connection_opts => {
+      #    :headers => {'Api-Key' => ENV['MMF_API_KEY']}
+      #  }
+      #}
 
       option :token_options, { :grant_type => 'authorization_code' }
 
-      uid{ raw_info['user_id'] }
+      uid{ raw_info['id'] }
 
       info{ raw_info }
 
